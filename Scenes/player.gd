180b5ffa27276682c016cf936166f01dd63b80b9
drw_xd <drw2007@icloud.com>
2025-02-16 17:43:53 -0500

    I uhh did stuff
    added gridmap and the like

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -19,12 +19,15 @@ func _unhandled_input(event):
 		head.rotate_y(-event.relative.x*sens)
 		camera.rotate_x(-event.relative.y*sens)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+	elif event is InputEventKey:
+		if event.keycode == KEY_ESCAPE:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var dir = (head.transform.basis*Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if dir:
