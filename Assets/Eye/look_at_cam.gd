96a1cd329c22e8f976fd274d759f7a336057c176
drw_xd <drw2007@icloud.com>
2025-02-16 17:46:14 -0500

    wau

--- Assets/Eye/look_at_cam.gd
+++ Assets/Eye/look_at_cam.gd
@@ -8,6 +8,6 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	cam = get_viewport().get_camera_3d()
 	look_at(cam.global_position)
